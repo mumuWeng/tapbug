bba9d15b33a0da386a2cbd619327ac4f8cc470e4
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2016-02-02 04:28:06 -0500

    code refactor

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
 function showStartPage () {
-	document.getElementById ("startPage").style = "display: block";
-	document.getElementById ("gamePage").style = "display: none";
+	document.getElementById("startPage").style = "display: block";
+	document.getElementById("gamePage").style = "display: none";
 }
 
 function showGamePage () {
-	if (document.getElementById ("level1").checked) {
-		showPage1();
-	}
-	if (document.getElementById ("level2").checked) {
-		showPage2();
-	}
+	showPage1();
 }
 
 function showPage1() {
-	document.getElementById ("startPage").style= "display: none;";
-	document.getElementById ("gamePage").style= "display: block;";
-	document.getElementById ("levelTitle").innerText= "Level 1";
+	document.getElementById("startPage").style= "display: none;";
+	document.getElementById("gamePage").style= "display: block;";
+	document.getElementById("levelTitle").innerText= "Level 1";
 	startGame(1);
 
 	// context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function showPage2() {
-	document.getElementById ("startPage").style= "display: none;";
-	document.getElementById ("gamePage").style= "display: block;";
-	document.getElementById ("levelTitle").innerText= "Level 2";
+	document.getElementById("startPage").style= "display: none;";
+	document.getElementById("gamePage").style= "display: block;";
+	document.getElementById("levelTitle").innerText= "Level 2";
 	startGame(2);
 
 	// context.clearRect(0, 0, canvas.width, canvas.height);
