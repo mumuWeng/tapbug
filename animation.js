2dccf4c049e38de6dc3880ce543355e7e40bb6d6
oOTKW <rlaxodn2528@gmail.com>
2016-02-02 17:06:45 -0500

    pause

--- animation.js
+++ animation.js
@@ -1,5 +1,7 @@
 var gameCanvas;
 var gameContext;
+var infoCanvas;
+var infoContext;
 var bugSizeR = 15;
 var foodSize = 25;
 var level;
@@ -23,10 +25,15 @@ function startGame(selectLevel) {
 		score2 = 0;
 	}
 
-	document.getElementById("levelTitle").innerText= "Level " + level;
+	infoCanvas = document.getElementById("infoCanvas");
+	infoContext = infoCanvas.getContext("2d");
+	infoCanvas.width = 400;
+	infoCanvas.height = 50;
+	//infoContext.fillStyle = "#0A2A0A";
+	//infoContext.fillRect(0,0,infoCanvas.width,infoCanvas.height);
+	
 	gameCanvas = document.getElementById("gameCanvas");
 	gameContext = gameCanvas.getContext("2d");
-
 	gameCanvas.width = 400;
 	gameCanvas.height = 600;
 	gameContext.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
@@ -34,7 +41,7 @@ function startGame(selectLevel) {
 	gameCanvas.addEventListener("mousedown", tapBug, false);
 
 	// Start timer
-	document.getElementById("time").innerHTML = time;
+	//document.getElementById("time").innerHTML = time;
 	timer = setInterval(displayTime, 1000);
 
 	// Put 5 foods
@@ -45,8 +52,19 @@ function startGame(selectLevel) {
 	createBug();
 }
 
+function pause() {
+	isPaused = !isPaused;
+	if (isPaused){
+		clearInterval(timer);	
+	} else {
+		timer = setInterval(displayTime, 1000);
+	}
+}
 
 function tapBug(event) {
+	if (isPaused)
+		return;
+
 	var x = event.offsetX;
     var y = event.offsetY;
 
@@ -77,21 +95,22 @@ function displayTime() {
 
 function createBug() {
 	createBugTimeout = setTimeout(function() {
-		var xPosition = parseInt(350*Math.random() + 25) //10~380 with bug's radius 15
-		var color = Math.random();
+		if (!isPaused) {
+			var xPosition = parseInt(350*Math.random() + 25) //10~380 with bug's radius 15
+			var color = Math.random();
 
-		if (color < 0.3) { //30%
-			color = "black";
-		} else if (color < 0.6) { //30%
-			color = "red";
-		} else { //40%
-			color = "orange";
+			if (color < 0.3) { //30%
+				color = "black";
+			} else if (color < 0.6) { //30%
+				color = "red";
+			} else { //40%
+				color = "orange";
+			}
+
+			var bug = new Bug(xPosition, color);
+			bugs.push(bug);
 		}
-
-		var bug = new Bug(xPosition, color);
-		bugs.push(bug);
 		createBug();
-
 	}, (2*Math.random() + 1) * 1000); // 1s ~ 3s
 }
 
@@ -100,8 +119,8 @@ var Bug = function (initialX, color) {
 	var y = 0;
 	var score;
 	var speed;
-	this.exist = true;
-	var removed = false;
+	this.exist = true; // alive. moving around
+	var removed = false; // completely removed. Fading out is not removed.
 
 	if (color == "black") {
 		score = 5;
@@ -117,10 +136,12 @@ var Bug = function (initialX, color) {
 	var moveInterval = setInterval(move, speed);
 	
 	function move() {
+		if (isPaused)
+			return;
+
 		gameContext.save();
 		clearArc(x, y);
 		
-
 		// move bug
 		var closestFood = findClosestFood(x, y);
 		if (closestFood) {
@@ -175,6 +196,8 @@ var Bug = function (initialX, color) {
 				if (!removed && alpha >= 0) {
 					drawBug(x, y, color, alpha);
 					fadeOut();
+				} else {
+					removed = true;
 				}
 			}, 100);
 		}
@@ -258,7 +281,6 @@ function clearGame() {
 
 function gameOver() {
 	clearGame();
-
 	if (level == 2) {
 		alert("Level 1 score: " + score1 + "\nLevel 2 score: " + score2);
 		showStartPage();
