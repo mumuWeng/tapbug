4798aa1d5eb9c130a163bcf1d151870c31a2386c
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2016-02-01 20:53:24 -0500

    fix level switch

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 function showGamePage () {
 	if (document.getElementById ("level1").checked) {
 		showPage1();
-		debugger
 	}
 	if (document.getElementById ("level2").checked) {
 		showPage2();
-		debugger
 	}
 }
 
 function showPage1 () {
 	document.getElementById ("startPage").style= "display: none;";
 	document.getElementById ("gamePage").style= "display: block;";
-	document.getElementById ("level1").style= "display: block;";
-	document.getElementById ("level2").style= "display: none;";
+	document.getElementById ("levelTitle").innerText= "Level 1";
+
 	context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function showPage2 () {
 	document.getElementById ("startPage").style= "display: none;";
 	document.getElementById ("gamePage").style= "display: block;";
-	document.getElementById ("level1").style= "display: none;";
-	document.getElementById ("level2").style= "display: block;";
+	document.getElementById ("levelTitle").innerText= "Level 2";
 	context.clearRect(0, 0, canvas.width, canvas.height);
 }
